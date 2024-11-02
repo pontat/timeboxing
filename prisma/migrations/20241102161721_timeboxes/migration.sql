18acf634172e200873e6ce4192e7d24fa47889b1
pontat <ponta.t.dev@gmail.com>
2024-11-03 01:25:09 +0900

    refactor: Timebox schema

--- prisma/migrations/20241102161721_timeboxes/migration.sql
+++ prisma/migrations/20241102161721_timeboxes/migration.sql
@@ -1,15 +1,15 @@
 -- CreateEnum
-CREATE TYPE "Status" AS ENUM ('idle', 'paused', 'completed');
+CREATE TYPE "TimeboxStatus" AS ENUM ('idle', 'running', 'paused', 'completed');
 
 -- CreateTable
 CREATE TABLE "timeboxes" (
     "id" UUID NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "comment" TEXT,
-    "start_time" TIMESTAMP(3) NOT NULL,
-    "end_time" TIMESTAMP(3) NOT NULL,
-    "remaining_time" INTEGER NOT NULL,
-    "status" "Status" NOT NULL DEFAULT 'idle',
+    "start_date_time" TIMESTAMP(3) NOT NULL,
+    "end_date_time" TIMESTAMP(3) NOT NULL,
+    "remaining_seconds" INTEGER NOT NULL,
+    "status" "TimeboxStatus" NOT NULL DEFAULT 'idle',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
